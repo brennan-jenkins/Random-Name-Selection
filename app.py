9c16e08b34b48d05ebabc44c836b98a87a904f41
Brennan Jenkins <brennan.jenkins@force24.co.uk>
2019-07-09 09:35:13 +0100

    minimised unnecessary code

--- app.py
+++ app.py
@@ -4,15 +4,5 @@
 
 import random
 
-
-def print_data(employee):
-    print(random.choice(employee))
-
-
-def main():
-    employee = ["adam", "Scott", "Michael", "Andrew", "Mark", "Fernando", "Faith", "Steve", "Lee", "Amani", "Liv", "Nick A", "James", "Jake", "Brett", "Graham", "Fraser", "Jacob", "Chelsea", "Phil", "George", "Charley", "Emma", "Steph"]
-    print_data(employee)
-
-
-main()
-
+employee = ["adam", "Scott", "Michael", "Andrew", "Mark", "Fernando", "Faith", "Steve", "Lee", "Amani", "Liv", "Nick A", "James", "Jake", "Brett", "Graham", "Fraser", "Jacob", "Chelsea", "Phil", "George", "Charley", "Emma", "Steph"]
+print(random.choice(employee))
